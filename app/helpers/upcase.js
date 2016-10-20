784e30520408f960aadefd49fddcdfa673b57cd9
Benigno Hernández H <bhernez@me.com>
2016-10-20 10:19:33 -0500

    * Coomment on hleper

--- app/helpers/upcase.js
+++ app/helpers/upcase.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export function upcase(params, hash) {
+	// Params tiene todos los argumentos enviados al helper separados por espacio
 	let text = params[0];
 
+	// Hash tiene todos los argumentso enviados al helper como llave=valor
 	if(hash.format === 'capital'){
 		return text.capitalize();
 	}
